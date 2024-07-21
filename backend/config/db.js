faed1981e409d0a43a58e77d15c7cbef36cd7080
mohamed salih va <salihva777@gmail.com>
2024-07-21 17:40:36 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-async function connectDB(){
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connected");
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI; // or wherever you are storing your URI
+    if (!uri) {
+      throw new Error('MONGODB_URI is not defined');
     }
-}
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 module.exports = connectDB;
