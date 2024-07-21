c71cff42dd5cde99f287cd8d3f9f2d61c1d24536
mohamed salih va <salihva777@gmail.com>
2024-07-21 19:56:26 +0530

    working

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URI = 'mongodb://localhost:27017/users'; 
+const MONGODB_URI = 'mongodb://localhost:27017/users';
 
 async function connectDB() {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log("Connected to MongoDB");
     } catch (err) {
         console.error("Error connecting to MongoDB:", err);
