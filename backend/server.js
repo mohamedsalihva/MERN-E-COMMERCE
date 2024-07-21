2be08d8c299ba3f2120141258febc076137fb0a0
mohamed salih va <salihva777@gmail.com>
2024-07-21 10:27:26 +0530

    better hosting

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
- const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const router = require('./routes/route');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
@@ -18,15 +17,15 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
- app.use(cookieParser())
+app.use(cookieParser());
 
 // Routes
 app.use("/api", router);
 
 // Connect to the database and start the server
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    app.listen(() => {
         console.log("Connected to DB");
-        console.log("Server is running on port " + PORT);
+        console.log("Server is running");
     });
 });
